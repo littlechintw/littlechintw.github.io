d570e8357ff2965c1e8e12eb54325ccf5a4561a4
Yong Cyun Huang <littlechintw@gmail.com>
2024-06-26 20:50:10 +0800

    Add photography

--- src/App.vue
+++ src/App.vue
@@ -280,6 +280,30 @@
         </dl>
       </div>
 
+    <div class="divider"></div>
+
+    <div style="margin-left: 10%; margin-right: 10%; margin-top: 20px;">
+      <div class="px-4 sm:px-0">
+
+        <kbd class="kbd kbd-md bg-black">
+          <h3 class="text-base font-semibold leading-7 text-gray-100"
+            style="background: linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722); -webkit-background-clip: text; -webkit-text-fill-color: transparent;">
+            Photography</h3>
+        </kbd>
+        <!-- <p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Work experience</p> -->
+      </div>
+      <div class="mt-6 border-t border-gray-600">
+        <dl class="divide-y divide-gray-700" display="flex" flex-wrap="wrap" justify-content="flex-start">
+          <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt class="text-sm font-medium leading-6 text-gray-100">上岸音樂節</dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-300 sm:col-span-2 sm:mt-0">
+              <div class="mx-2 badge badge-accent badge-outline">2024</div> 相手
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+
 
     </div>
 
