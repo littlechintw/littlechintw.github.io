c9e2802e3d1ae71079050b49e25b3625f12e13cd
littlechin <littlechintw@gmail.com>
2024-06-27 00:15:50 +0800

    Fix layout

--- src/App.vue
+++ src/App.vue
@@ -200,7 +200,7 @@
             </dd>
             <dt class="text-sm font-medium leading-6 text-gray-100"></dt>
             <dd class="mt-1 text-sm leading-6 text-gray-300 sm:col-span-2 sm:mt-0">
-              <div class="mx-2 badge badge-accent badge-outline">2024</div> Hackathon 紀錄組 組員
+              <div class="mx-2 badge badge-accent">2024</div> Hackathon 紀錄組 組員
             </dd>
           </div>
           <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -222,7 +222,7 @@
             </dd>
             <dt class="text-sm font-medium leading-6 text-gray-100"></dt>
             <dd class="mt-1 text-sm leading-6 text-gray-300 sm:col-span-2 sm:mt-0">
-              <div class="mx-2 badge badge-accent badge-outline">2024</div> 紀錄組 組員
+              <div class="mx-2 badge badge-accent">2024</div> 紀錄組 組員
             </dd>
           </div>
           <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -244,7 +244,7 @@
             </dd>
             <dt class="text-sm font-medium leading-6 text-gray-100"></dt>
             <dd class="mt-1 text-sm leading-6 text-gray-300 sm:col-span-2 sm:mt-0">
-              <div class="mx-2 badge badge-accent badge-outline">2024</div> 紀錄組 組員
+              <div class="mx-2 badge badge-accent">2024</div> 紀錄組 組員
             </dd>
           </div>
           <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -279,6 +279,7 @@
           </div>
         </dl>
       </div>
+    </div>
 
     <div class="divider"></div>
 
@@ -304,9 +305,6 @@
       </div>
     </div>
 
-
-    </div>
-
   </body>
 
   <footer class="mt-2 footer items-center p-4 bg-neutral text-neutral-content">
@@ -322,8 +320,8 @@
       </p>
     </aside>
     <nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-      <a href="https://www.linkedin.com/in/littlechin" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24"
-          height="24" viewBox="0 0 24 24" class="fill-current">
+      <a href="https://www.linkedin.com/in/littlechin" target="_blank"><svg xmlns="http://www.w3.org/2000/svg"
+          width="24" height="24" viewBox="0 0 24 24" class="fill-current">
           <path
             d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
         </svg>
