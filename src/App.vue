f75adfacdc8cdd1d2ee60a2631234d2b1693594b
Yong Cyun Huang <littlechintw@gmail.com>
2025-12-23 16:41:01 +0800

    Add BubbleBackground component to App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   <body>
     <div class="bg-black">
       <div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+        <BubbleBackground />
         <!-- <img src="{img url}" alt=""
           class="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center" /> -->
         <div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl animate-pulse-slow"
@@ -16,7 +17,7 @@
           <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
             style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
         </div>
-        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="relative z-10 mx-auto max-w-7xl px-6 lg:px-8">
           <div class="mx-auto max-w-2xl lg:mx-0 fade-in-element">
             <div class="avatar">
               <div class="w-24 rounded-xl hover-scale">
@@ -187,6 +188,7 @@
 <script setup>
 import { reactive, onMounted } from 'vue'
 import jsonData from './data.json'
+import BubbleBackground from './components/BubbleBackground.vue'
 
 const data = reactive(jsonData)
 
